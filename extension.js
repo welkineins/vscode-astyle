95e2bb2bdc01476c362d7a3c0b27627bf42c6af0
Chieh Yu <welkineins@gmail.com>
2016-08-27 21:08:42 +0800

    finish prototyping

--- extension.js
+++ extension.js
@@ -1,30 +1,98 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
+const vscode         = require('vscode');
+const fs             = require('fs');
+const path           = require('path');
+const childProcess   = require('child_process');
+const diffMatchPatch = require('diff-match-patch');
+
+class AstyleFormatter {
+    // interface required by vscode.DocumentFormattingEditProvider
+    provideDocumentFormattingEdits(document, options, token) {
+        return new Promise((resolve, reject) => {
+            let astyleBinPath = "D:\\AStyle.exe";
+            let args = [];
+            let astyle = childProcess.spawn(astyleBinPath, args);
+            let out = "", err = "";
+
+            astyle.stdin.write(document.getText());
+            astyle.stdin.end();
+
+            astyle.stdout.on('data', (data) => {
+                out += data;
+            });
+
+            astyle.stderr.on('data', (data) => {
+                err += data;
+            });
+
+            astyle.on('close', (code) => {
+                if (code !== 0) {
+                    reject('Failed to format file. (code: ' + code + ', reason: ' + err + ')');
+                } else {
+                    resolve(this.generateTextEditors(document, out));
+                }
+            });
+
+            astyle.on('error', (err) => {
+                if (err.code == 'ENOENT') {
+                    vscode.window.showErrorMessage('Can\'t found astyle.');
+                } else {
+                    vscode.window.showErrorMessage('Failed to launch astyle. (code: ' + err.code + ')');
+                }
+
+                reject(err);
+            });
+        });
+    }
+
+    generateTextEditors(document, formattedText) {
+        let dmp = new diffMatchPatch.diff_match_patch();
+        let diffs = dmp.diff_main(document.getText(), formattedText);
+        let editors = [];
+        let line = 0, character = 0;
+
+        diffs.forEach((diff) => {
+            let op = diff[0];
+            let text = diff[1];
+            let start = new vscode.Position(line, character);
+
+            for(let c of text) {
+                if (c === '\n') {
+                    character = 0;
+                    line++;
+                } else {
+                    character++;
+                }
+            }
+
+            switch(op) {
+                case diffMatchPatch.DIFF_INSERT:
+                    editors.push(vscode.TextEdit.insert(start, text));
+                    // this new part should not affect counting of original document
+                    character = start.character;
+                    line = start.line;
+                    break;
+                case diffMatchPatch.DIFF_DELETE:
+                    let end = new vscode.Position(line, character);
+                    editors.push(vscode.TextEdit.delete(new vscode.Range(start, end)));
+                    break;
+                case diffMatchPatch.DIFF_EQUAL:
+                    break;
+            }
+        });
+
+        return editors;
+    }
+};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
+    let formatter = new AstyleFormatter();
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-astyle" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    ["cpp", "c", "objective-c", "csharp", "java"].forEach(function(language) {
+        let disposable = vscode.languages.registerDocumentFormattingEditProvider(language, formatter);
+        context.subscriptions.push(disposable);
     });
-
-    context.subscriptions.push(disposable);
 }
+
 exports.activate = activate;
-
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
